b55dbe1ed2d081ddb21967690e98201875598436
babinogard <bartlomiejkiczka@gamil.com>
2025-10-14 22:20:48 +0200

    Definition of calc_surface function

--- git_test.py
+++ git_test.py
@@ -1,8 +1,33 @@
-print("hello user!")
 
-print("Please type in your parameters. This will let us complete your calculations:)")
+def user_interface(): #This function is user interface. It gets dimmensions from user to calcualte surface later.
 
-length, width, height = int(input("Length of room (m) : ")), int(input("Width of room (m) : ")), int(input("Height of room (m) : "))
+    print("hello user!\nPlease type in your parameters.\nThis will allow us to complete your calculations:)")
+    length, width, height = float(input("Length of room (m) : ")),float(input("Width of room (m) : ")),float(input("Height of room (m) : "))
+    print(f"These are dimmensions of your room: {length}x{width}x{height}m")
 
-print(f"These are dimmensions of your room: {length}x{width}x{height}m")
+    #print(user_interface())
+
+    #print(f"These are dimmensions of your room: {length}x{width}x{height}m")
+
+    #def my_func():
+    #    return "Python", 42, [1,2,3]
+
+    #print(my_func())
+
+    return length, width, height
+ 
+
+def calc_surface():
+
+    length, width, height = user_interface()
+
+    walls = length*height*2+width*height*2
+    ceilling = length*width
+    floor = length*width
+
+    return walls, ceilling, floor
+
+
+print(calc_surface())
+
 
